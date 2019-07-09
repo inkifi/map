94c4172b4d361b540172dda17bc2866c6cbbe945
Dmitry Fedyuk <dfediuk@gmail.com>
2019-07-10 02:45:34 +0600

    no message

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,3 @@
+var config = {paths: {
+	'Inkifi_Map/carousel': 'Inkifi_Map/lib/carousel/main'
+}};
---
+++ view/frontend/web/index.js
@@ -0,0 +1,10 @@
+// 2019-07-09
+define(['jquery', 'domReady!', 'Inkifi_Map/carousel'], function($) {return (
+/**
+ * @param {Object} c
+ * @param {HTMLDivElement} e
+ */
+function(c, e) {
+	var $e = $(e);
+	$('.ikf-cities', $e).slick();
+});});
