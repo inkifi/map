82a2bf0acb6f39d6f1bfb5a251e46eb8a29c2621
Dmitry Fedyuk <dfediuk@gmail.com>
2019-07-15 12:29:25 +0600

    no message

--- view/frontend/web/index.js
+++ view/frontend/web/index.js
@@ -6,13 +6,21 @@ define(['jquery', 'domReady!', 'Inkifi_Map/carousel'], function($) {return (
  */
 function(c, e) {
 	var $e = $(e);
-	$('.ikf-cities', $e).slick({
-		// 2019-07-15
-		// «Enables centered view with partial prev/next slides.
-		// Use with odd numbered slidesToShow counts.»
-		centerMode: true
-		,infinite: true
-		,slidesToScroll: 3 // 2019-07-10 «# of slides to scroll at a time»
-		,slidesToShow: 5.7  // 2019-07-10 «# of slides to show at a time»
-	});
+	(function() {
+		var $e = $('.ikf-cities', $e);
+		var slidesToShow = function() {
+			var w = $(window).width();
+			return (w > 1739 ? 5.7 : (w > 1469 ? 4.7 : (w > 1024 ? 3.8 : (w > 640 ? 3.3 : 1.9))));
+		};
+		$e.slick({
+			// 2019-07-15
+			// «Enables centered view with partial prev/next slides.
+			// Use with odd numbered slidesToShow counts.»
+			centerMode: true
+			,infinite: true
+			,slidesToScroll: 3 // 2019-07-10 «# of slides to scroll at a time»
+			,slidesToShow: slidesToShow()  // 2019-07-10 «# of slides to show at a time»
+		});
+		$(window).resize(function() {$e.slick('slickSetOption', 'slidesToShow', slidesToShow(), false);});
+	})();
 });});
