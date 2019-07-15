7b3bf0c1e95bf800769ee8660fcb969b97eba85a
Dmitry Fedyuk <dfediuk@gmail.com>
2019-07-15 11:52:35 +0600

    no message

--- view/frontend/web/index.js
+++ view/frontend/web/index.js
@@ -7,9 +7,12 @@ define(['jquery', 'domReady!', 'Inkifi_Map/carousel'], function($) {return (
 function(c, e) {
 	var $e = $(e);
 	$('.ikf-cities', $e).slick({
-		infinite: true
+		// 2019-07-15
+		// «Enables centered view with partial prev/next slides.
+		// Use with odd numbered slidesToShow counts.»
+		centerMode: true
+		,infinite: true
 		,slidesToScroll: 3 // 2019-07-10 «# of slides to scroll at a time»
-		,slidesToShow: 5.5  // 2019-07-10 «# of slides to show at a time»
-		,centerMode: true
+		,slidesToShow: 5.7  // 2019-07-10 «# of slides to show at a time»
 	});
 });});
