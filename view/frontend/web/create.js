49b54fd80908bb9c1ec9fc167fd7faa74ac49345
Dmitry Fedyuk <dfediuk@gmail.com>
2019-07-16 18:53:58 +0600

    no message

---
+++ view/frontend/web/create.js
@@ -0,0 +1,11 @@
+// 2019-07-16
+define(['jquery', 'domReady!'], function($) {return (
+/**
+ * @param {Object} c
+ * @param {HTMLDivElement} e
+ */
+function(c, e) {
+	var $e = $(e);
+	(function() {
+	})();
+});});
