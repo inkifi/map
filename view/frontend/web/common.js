327b9eb714606889ec4b7efab81238c38d995873
Dmitry Fedyuk <dfediuk@gmail.com>
2019-08-06 05:30:20 +0600

    no message

--- view/frontend/web/common.js
+++ view/frontend/web/common.js
@@ -5,14 +5,18 @@ define(['jquery', 'domReady!'], function($) {return (
  * @param {HTMLDivElement} e
  */
 function(c, e) {
-	var $e = $(e);
 	(function() {
 		var $menu = $('.ikf-menu');
 		var $page = $('.page-wrapper');
-		$('.ikf-burger > a', $e).click(function() {
+		$('.ikf-burger > a').click(function() {
 			$(this).toggleClass('ikf-active');
 			$menu.toggleClass('ikf-active');
 			$page.toggleClass('ikf-mobile-menu');
 		});
+	})();
+	(function() {
+		var $inputs = $('input', $('.inkifi-map-index-index, .inkifi-map-create-index'));
+		var f = function() {var $e = $(this); $e.toggleClass('ikf-has-value', '' !== $e.val())};
+		$inputs.each(f).on('change paste keyup', f);
 	})();
 });});
