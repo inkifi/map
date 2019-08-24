44ab72f28b79b75e3e9698d79c176224fb3238a5
Dmitry Fedyuk <dfediuk@gmail.com>
2019-08-24 19:49:35 +0600

    no message

--- view/frontend/web/js/create/geocode.js
+++ view/frontend/web/js/create/geocode.js
@@ -4,17 +4,12 @@ define(['df-lodash', 'jquery'], function(_, $) {return (function() {
 	const $iH1 = $('input[name=h1]');
 	const $iH2 = $('input[name=h2]');
 	const $iH3 = $('input[name=h3]');
+	const EVENT = {coordinatesChanged: 'inkifi.map.coordinatesChanged'};
 	(function() {
 		const $label = $('article .ikf-label', $editor);
 		const $h1 = $('h1', $label);
 		const $h2 = $('h2', $label);
 		const $h3 = $('h3', $label);
-		$iH1.add($iH2).add($iH3).on('keyup paste', function() {$(this).change();});
-		$iH1.on('change', function() {$h1.html($(this).val());});
-		$iH2.change(function() {$h2.html($(this).val());});
-		$iH3.change(function() {$h3.html($(this).val());});
-	})();
-	(function() {
 		const KEY = '6b0d03206e1b4d9f812be0b8c1a4475c';
 		const URL = 'https://api.opencagedata.com/geocode/v1/json';
 		const formatCity = function(v) {return v !== 'Palma' ? v : 'Palma de Mallorca';};
@@ -30,29 +25,33 @@ define(['df-lodash', 'jquery'], function(_, $) {return (function() {
 				}
 			})
 		);});};
-		$('button.ikf-location').click(function(e) {
-			e.preventDefault();
-			navigator.geolocation.getCurrentPosition(function(r) {
-				const lat = r.coords.latitude;
-				const lng = r.coords.longitude;
-				/**
-				 * 2019-08-14
-				 * Starting from Firefox 34 / Chrome 41 / Safari 9 / Microsoft Edge
-				 * you can use an ES2015 / ES6 feature called Template Literals:
-				 * https://stackoverflow.com/a/28088965
-				 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-				 * https://caniuse.com/#search=Template%20Literals
-				 * The plain JS code is:
-				 * 		''.concat(Number(lat).toFixed(3), "\xb0N/").concat(Number(lng).toFixed(3), "\xb0E")
-				 */
-				var coord = `${Number(lat).toFixed(3)}°N/${Number(lng).toFixed(3)}°E`;
-				// 2019-08-14 `.val()` does not trigger `change`: https://stackoverflow.com/a/3179392
-				$iH3.val(coord).change();
-				getGeocodeByCoords(lat, lng).then((r) => {
-					$iH1.val(r.city).change();
-					$iH2.val(r.country).change();
-				});
-			});
+		$iH1.add($iH2).add($iH3).on('keyup paste', function() {$(this).change();});
+		$iH1.on('change', function() {$h1.html($(this).val());});
+		$iH2.change(function() {$h2.html($(this).val());});
+		$iH3.change(function() {$h3.html($(this).val());});
+		$(window).bind(EVENT.coordinatesChanged, function(e, lat, lng) {
+			/**
+			 * 2019-08-14
+			 * 1) Starting from Firefox 34 / Chrome 41 / Safari 9 / Microsoft Edge
+			 * you can use an ES2015 / ES6 feature called Template Literals:
+			 * https://stackoverflow.com/a/28088965
+			 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+			 * https://caniuse.com/#search=Template%20Literals
+			 * The plain JS code is:
+			 * 		''.concat(Number(lat).toFixed(3), "\xb0N/").concat(Number(lng).toFixed(3), "\xb0E")
+			 * 2) 2019-08-14 `.val()` does not trigger `change`: https://stackoverflow.com/a/3179392
+			 */
+			$iH3.val(`${Number(lat).toFixed(3)}°N/${Number(lng).toFixed(3)}°E`).change();
+			getGeocodeByCoords(lat, lng).then((r) => {
+				$iH1.val(r.city).change();
+				$iH2.val(r.country).change();
+			});			
 		});
 	})();
+	$('button.ikf-location').click(function(e) {
+		e.preventDefault();
+		navigator.geolocation.getCurrentPosition(function(r) {
+			$(window).trigger(EVENT.coordinatesChanged, [r.coords.latitude, r.coords.longitude]);
+		});
+	});
 })();});
