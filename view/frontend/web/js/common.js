fdd076f2672a646cbbe309cf80faa63526d15834
Dmitry Fedyuk <dfediuk@gmail.com>
2019-08-27 17:12:28 +0600

    no message

--- view/frontend/web/js/common.js
+++ view/frontend/web/js/common.js
@@ -1,16 +1,12 @@
 // 2019-07-22
-define(['jquery', 'domReady!'], function($) {return (function() {
-	(() => {
-		const $menu = $('.ikf-menu');
-		const $page = $('.page-wrapper');
-		// 2019-08-27
-		// The arrow function syntax breaks `this` in jQuery event handlers:
-		// https://stackoverflow.com/questions/27670401
-		$('.ikf-burger > a').click(function() {
-			const c = 'ikf-active';
-			$(this).toggleClass(c);
-			$menu.toggleClass(c);
-			$page.toggleClass('ikf-mobile-menu');
-		});
-	})();
-});});
+require(['jquery', 'domReady!'], $ => {
+	const $menu = $('.ikf-menu');
+	const $page = $('.page-wrapper');
+	// 2019-08-27
+	// The arrow function syntax breaks `this` in jQuery event handlers:
+	// https://stackoverflow.com/questions/27670401
+	$('.ikf-burger > a').click(function() {
+		$(this).add($menu).toggleClass('ikf-active');
+		$page.toggleClass('ikf-mobile-menu');
+	});
+});
