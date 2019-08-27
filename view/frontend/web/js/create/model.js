51bee1980eb784b70fa1d50d02a9bff162af194d
Dmitry Fedyuk <dfediuk@gmail.com>
2019-08-27 07:53:51 +0600

    no message

--- view/frontend/web/js/create/model.js
+++ view/frontend/web/js/create/model.js
@@ -2,26 +2,26 @@
 define([
 	'df', 'df-lodash', 'ko'
 	,'Df_Core/thirdParty/URI/URI', 'Inkifi_Map/js/create/lib/geocode'
-], function(df, _ , ko, URI, geocode) {return {
-	_init: function() {
+], (df, _ , ko, URI, geocode) => ({
+	_init() {
 		var _this = this;
-		this.pos = ko.observable(function() {
+		this.pos = ko.observable((() => {
 			var q = _this.q();
 			return q.latitude && q.longitude
 				? {lat: q.latitude, lng: q.longitude}
 				: {lat: 51.487971262617, lng: -0.074918480033487}
 			;
-		}());
-		this.posS = ko.pureComputed(function() {
-			var v = this.pos();
+		})());
+		this.posS = ko.pureComputed(() => {
+			var v = _this.pos();
 			return `${Number(v.lat).toFixed(3)}°N/${Number(v.lng).toFixed(3)}°E`;
-		}, this);
+		});
 		this.h1 = ko.observable();
 		this.h2 = ko.observable();
 		this.h3 = ko.observable();
 		this.updateH3();
-		this.pos.subscribe(function(v) {
-			geocode(v.lat, v.lng).then(function(r) {_this.h1(r.city); _this.h2(r.country);});
+		this.pos.subscribe(v => {
+			geocode(v.lat, v.lng).then(r => {_this.h1(r.city); _this.h2(r.country);});
 		});
 		return this;
 	},
@@ -34,11 +34,11 @@ define([
 	 * It returns an object hash. The hash is empty if the URL does not contain the `?...` part.
 	 * @returns {Object}
 	 */
-	q: _.once(function() {return URI.parseQuery(location.search);}),
+	q: _.once(() => URI.parseQuery(location.search)),
 	/**
 	 * 2019-08-27
 	 * @used-by _init()
 	 * @used-by Inkifi_Map/js/create::locate()
 	 */
-	updateH3: function() {this.h3(this.posS());}
-}._init();}); // 2019-08-26 https://stackoverflow.com/a/4616273
+	updateH3() {this.h3(this.posS());}
+})._init()); // 2019-08-26 https://stackoverflow.com/a/4616273
