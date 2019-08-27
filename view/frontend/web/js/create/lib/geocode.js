85f3110d039a48ea5b174a3e68d920fd84bf0dc2
Dmitry Fedyuk <dfediuk@gmail.com>
2019-08-27 17:20:35 +0600

    no message

--- view/frontend/web/js/create/lib/geocode.js
+++ view/frontend/web/js/create/lib/geocode.js
@@ -1,18 +1,18 @@
 // 2019-08-24
-define(['df-lodash'], function(_) {
+define(['df-lodash'], _ => {
+	const fCity = v => v !== 'Palma' ? v : 'Palma de Mallorca';
+	const fCountry = v => v !== 'PRC' ? v : 'China';
 	const KEY = '6b0d03206e1b4d9f812be0b8c1a4475c';
 	const URL = 'https://api.opencagedata.com/geocode/v1/json';
-	const formatCity = function(v) {return v !== 'Palma' ? v : 'Palma de Mallorca';};
-	const formatCountry = function(v) {return v !== 'PRC' ? v : 'China';};
-	return async(lat, lng) => {return new Promise((resolve) => {return(
-		// 2019-08-14
-		// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
-		// https://caniuse.com/#search=fetch
-		fetch(`${URL}?q=${lat}+${lng}&key=${KEY}&language=en`).then((r) => r.json()).then((r) => {
+	// 2019-08-14
+	// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+	// https://caniuse.com/#search=fetch	
+	return async(lat, lng) => new Promise(resolve =>
+		fetch(`${URL}?q=${lat}+${lng}&key=${KEY}&language=en`).then(r => r.json()).then(r => {
 			const c = _.get(r, 'results[0].components');
 			if (c) {
-				resolve({city: formatCity(c.city), country: formatCountry(c.country), state: c.state});
+				resolve({city: fCity(c.city), country: fCountry(c.country), state: c.state});
 			}
 		})
-	);});};
+	);
 });
