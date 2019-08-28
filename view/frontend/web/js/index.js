287e222f140912dfdaa1869643a2a5201614e453
Dmitry Fedyuk <dfediuk@gmail.com>
2019-08-28 14:32:57 +0600

    no message

--- view/frontend/web/js/index.js
+++ view/frontend/web/js/index.js
@@ -16,6 +16,10 @@ require(['jquery', 'Inkifi_Map/lib/carousel/main', 'domReady!'], $ => {
 		,infinite: true
 		,slidesToScroll: 3 // 2019-07-10 «# of slides to scroll at a time»
 		,slidesToShow: slidesToShow()  // 2019-07-10 «# of slides to show at a time»
+		// 2019-08-28
+		// 1) «Allow users to drag or swipe directly to a slide irrespective of slidesToScroll».
+		// 2) It allows us to drag a particular slide to a desired place by a mouse.
+		,swipeToSlide: true
 	});
 	$(window).resize(() => {$e.slick('slickSetOption', 'slidesToShow', slidesToShow(), false);});
 });
