80dc519b536e00e4049a593482afa24759b7656b
Dmitry Fedyuk <dfediuk@gmail.com>
2019-08-30 20:06:31 +0600

    no message

--- view/frontend/web/js/create.js
+++ view/frontend/web/js/create.js
@@ -5,8 +5,22 @@ define([
 	,'Inkifi_Map/js/create/module/mapbox'
 	,'domReady!'
 ], (_p, _m, $) => _p.extend(_m).extend({
-	initialize() {
-		this._super();
+    /**
+	 * 2019-08-27
+	 * @used-by https://github.com/inkifi/map/blob/0.0.7/view/frontend/templates/create/form/location.phtml#L1
+	 */
+	locate() {navigator.geolocation.getCurrentPosition(_.bind(r =>
+		this.pos({lat: r.coords.latitude, lng: r.coords.longitude})
+	, this));},
+	/**
+	 * 2019-08-30
+	 * 1) The `scope` binding destroys all previously set jQuery event bindings:
+	 * 		ko.utils.arrayForEach(el.childNodes, ko.cleanNode);
+	 * https://github.com/magento/magento2/blob/2.3.2/app/code/Magento/Ui/view/base/web/js/lib/knockout/bindings/scope.js#L32
+	 * So we can not bind jQuery event handlers in the compontent's `initialize()` method,
+	 * and we use the `afterRender` function instead.
+	 */
+	onRender() {
 		(() => {
 			const $colors = $('input[name="color"]');
 			const $frames = $('input[name="frame"]');
@@ -34,15 +48,7 @@ define([
 				}
 			});
 		})();
-		return this;
-	}, 
-    /**
-	 * 2019-08-27
-	 * @used-by https://github.com/inkifi/map/blob/0.0.7/view/frontend/templates/create/form/location.phtml#L1
-	 */
-	locate() {navigator.geolocation.getCurrentPosition(_.bind(r =>
-		this.pos({lat: r.coords.latitude, lng: r.coords.longitude})
-	, this));},
+	},
     /**
 	 * 2019-08-28
 	 * @used-by https://github.com/inkifi/map/blob/0.0.9/view/frontend/templates/create/article.phtml#L4-L6
